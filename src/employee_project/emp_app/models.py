0d6b544c984834d3578b0eac739d251abe7a4e18
ashu <ashu@gmail.com>
2020-11-05 16:15:10 +0530

    apply migrations

--- src/employee_project/emp_app/models.py
+++ src/employee_project/emp_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Salary(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Employee(models.Model):
+    name = models.CharField(max_length=100)
+    Designation = models.CharField(max_length=100)
+    salary = models.ForeignKey(Salary, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
